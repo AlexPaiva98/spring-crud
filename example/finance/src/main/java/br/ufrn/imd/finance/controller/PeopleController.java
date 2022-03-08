9d1dd7843618492ec59b05670b1b3644d5d85bee
Alex Paiva <a.s.paiva98@gmail.com>
2022-03-08 15:09:50 -0300

    Corrige um detalhe

--- example/finance/src/main/java/br/ufrn/imd/finance/controller/PeopleController.java
+++ example/finance/src/main/java/br/ufrn/imd/finance/controller/PeopleController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ufrn.imd.springcrud.controller.GenericController;
 import br.ufrn.imd.springcrud.service.GenericService;
 
+import br.ufrn.imd.finance.model.PeopleModel;
+import br.ufrn.imd.finance.model.dto.PeopleDto;
 import br.ufrn.imd.finance.service.PeopleService;
 
 @RestController
 @RequestMapping("/finances")
-public class PeopleController extends GenericController {
+public class PeopleController extends GenericController<PeopleModel, PeopleDto> {
     private PeopleService peopleService;
 
     @Autowired
